7a74a19db7b69ace787a499452b51072c5be5169
Jol Viali <viali753@gmail.com>
2023-05-15 22:45:13 -0300

    aulas 05 06 e 07 selenium anotacoes e seletores

--- src/main/java/br/com/jol/media/Media.java
+++ src/main/java/br/com/jol/media/Media.java
@@ -1,2 +1,12 @@
-package br.com.jol.media;public class Media {
+package br.com.jol.media;
+
+public class Media {
+
+    public String calculaMedia(double v, double v1) {
+        double media = (v + v1) / 2;
+        if (media < 5) {
+            return "Reprovado";
+        }
+        return "Aprovado";
+    }
 }
