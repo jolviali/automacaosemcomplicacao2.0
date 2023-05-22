cb245f4791c3d8d751eb17a931aab169df6a567f
Jol Viali <viali753@gmail.com>
2023-05-22 20:28:12 -0300

    aula 13 e 14, cucumber e runner

---
+++ src/test/java/br/com/jol/runner/RunnerTest.java
@@ -0,0 +1,17 @@
+package br.com.jol.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        tags = "",
+        glue = "src/test/java/br/com/jol/steps",
+        plugin = {"json:target/reports/cucumberReports.json", "pretty"},
+        snippets = CucumberOptions.SnippetType.CAMELCASE
+)
+public class RunnerTest {
+
+}
